652a4386484e9d9ac592c5161f10720eb8bc2c98
StanleyL1 <Stanleylarin011@gmail.com>
2023-02-22 14:17:59 -0600

    Primer Parcial

--- Style.js
+++ Style.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
     particles: {
       number: { value: 6, density: { enable: true, value_area: 800 } },
-      color: { value: "#d900f7" },
+      color: { value: "#ee00ff" },
       shape: {
         type: "polygon",
         stroke: { width: 0, color: "#000" },
@@ -9,12 +9,12 @@ particlesJS("particles-js", {
         image: { src: "img/github.svg", width: 100, height: 100 }
       },
       opacity: {
-        value: 0.42536431666010227,
+        value: 0.3,
         random: true,
         anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
       },
       size: {
-        value: 106.34107916502555,
+        value: 160,
         random: false,
         anim: { enable: true, speed: 10, size_min: 40, sync: false }
       },
@@ -54,10 +54,18 @@ particlesJS("particles-js", {
     retina_detect: true
   });
   var count_particles, stats, update;
-  
+  stats = new Stats();
+  stats.setMode(0);
+  stats.domElement.style.position = "absolute";
+  stats.domElement.style.left = "0px";
+  stats.domElement.style.top = "0px";
+  document.body.appendChild(stats.domElement);
   count_particles = document.querySelector(".js-count-particles");
   update = function () {
+    stats.begin();
+    stats.end();
     if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
     }
     requestAnimationFrame(update);
   };
